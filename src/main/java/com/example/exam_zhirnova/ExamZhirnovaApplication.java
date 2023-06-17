7c897b0c398935818a089d0b91a6e5fbbc96114d
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2023-06-17 23:15:38 +0300

    Initial Commit

---
+++ src/main/java/com/example/exam_zhirnova/ExamZhirnovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_zhirnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamZhirnovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamZhirnovaApplication.class, args);
+    }
+
+}
